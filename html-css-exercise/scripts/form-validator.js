48292b1be60dd8ecc072b2aa891ba3c41aca5901
ankoyeeri <ray.zug@gmail.com>
2023-04-11 13:47:18 +0300

    feat: add form-validator class declarations

---
+++ html-css-exercise/scripts/form-validator.js
@@ -0,0 +1,39 @@
+export class FormValidator {
+    formName = "";
+    formFields = [];
+
+    constructor(formName, ...formFields) {
+        this.formName = formName;
+        this.formFields = formFields;
+    }
+
+    validate() {
+        for (let formField of this.formFields) {
+            if (!formField.validator()) throw new Error(`${this.formName}: <${formField.name}> form field is not valid`);
+        }
+    }
+}
+
+export class FormField {
+    #name = "";
+    #value = undefined;
+    #validator = undefined;
+
+    get name() {
+        return this.#name;
+    }
+
+    get value() {
+        return this.#value;
+    }
+
+    get validator() {
+        return this.#validator;
+    }
+
+    constructor(name, value, validator) {
+        this.#name = name;
+        this.#value = value;
+        this.#validator = validator;
+    }
+}
