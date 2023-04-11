92359d5480894beff46a5ad94d7e8784a21bf8e8
ankoyeeri <ray.zug@gmail.com>
2023-04-11 13:48:22 +0300

    feat: add login.html form validation

---
+++ html-css-exercise/scripts/login.js
@@ -0,0 +1,63 @@
+import AlertErrorHandler from "./error.js";
+import {FormValidator, FormField } from "./form-validator.js";
+
+function validateRegisterForm() {
+  const form = document.getElementById("register-form");
+  const formData = new FormData(form);
+
+  let formValidator = new FormValidator( 
+    'Register',
+    new FormField('name', formData.get('name'), function () {
+        return this.value && this.value.length > 1;
+    }),
+    new FormField('surname', formData.get('surname'), function () {
+        return this.value.length > 1;
+    }),
+    new FormField('email', formData.get('email'), function () {
+      return this.value && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g.test(this.value);
+    }),
+    new FormField('emailAgain', formData.get('emailAgain'), function () {
+        return this.value && this.value === formData.get('email');
+    }),
+    new FormField('password', formData.get('password'), function () {
+        return this.value && this.value.length > 7;
+    }),
+    new FormField('passwordAgain', formData.get('passwordAgain'), function () {
+        return this.value && this.value === formData.get('password');
+    }),
+  );
+
+  try {
+    formValidator.validate();
+    window.location.href = 'home.html'
+  } catch (error) {
+    AlertErrorHandler.raise(error);
+  }
+}
+
+function validateLoginForm() {
+    const form = document.getElementById('sign-in-form');
+    const formData = new FormData(form);
+
+    let formValidator = new FormValidator(
+        'Login',
+        new FormField('email', formData.get('email'), function() {
+            return this.value && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g.test(this.value);
+        }),
+        new FormField('password', formData.get('password'), function() {
+            return this.value;
+        }),
+    );
+
+    try {
+        formValidator.validate();
+        window.location.href = 'home.html';
+    } catch (error) {
+        AlertErrorHandler.raise(error);
+    }
+}
+
+
+
+window.validateLoginForm = validateLoginForm;
+window.validateRegisterForm = validateRegisterForm;
