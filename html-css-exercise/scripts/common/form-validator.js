3c6ba870073090a20e4b02f2b7dbd9b4799d579f
ankoyeeri <ray.zug@gmail.com>
2023-04-13 11:51:29 +0300

    docs: describe FormValidator class

--- html-css-exercise/scripts/common/form-validator.js
+++ html-css-exercise/scripts/common/form-validator.js
@@ -1,12 +1,23 @@
+/**
+ * @class FormData validation
+ */
 export class FormValidator {
   formName = "";
   formFields = [];
 
+  /**
+   * @param {string} formName Form name
+   * @param  {...FormField} formFields Form fields
+   */
   constructor(formName, ...formFields) {
     this.formName = formName;
     this.formFields = formFields;
   }
 
+  /**
+   * Validate passed form data sequently.
+   * In case if any of form fields does not pass the validation, the Error will be thrown.
+   */
   validate() {
     for (let formField of this.formFields) {
       if (!formField.validator())
