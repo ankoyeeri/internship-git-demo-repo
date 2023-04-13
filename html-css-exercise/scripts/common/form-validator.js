33124046206b4f0cbe55512385489b573b40278c
ankoyeeri <ray.zug@gmail.com>
2023-04-13 11:42:40 +0300

    style: use Prettier formatting

--- html-css-exercise/scripts/common/form-validator.js
+++ html-css-exercise/scripts/common/form-validator.js
@@ -1,39 +1,42 @@
 export class FormValidator {
-    formName = "";
-    formFields = [];
+  formName = "";
+  formFields = [];
 
-    constructor(formName, ...formFields) {
-        this.formName = formName;
-        this.formFields = formFields;
-    }
+  constructor(formName, ...formFields) {
+    this.formName = formName;
+    this.formFields = formFields;
+  }
 
-    validate() {
-        for (let formField of this.formFields) {
-            if (!formField.validator()) throw new Error(`${this.formName}: <${formField.name}> form field is not valid`);
-        }
+  validate() {
+    for (let formField of this.formFields) {
+      if (!formField.validator())
+        throw new Error(
+          `${this.formName}: <${formField.name}> form field is not valid`
+        );
     }
+  }
 }
 
 export class FormField {
-    #name = "";
-    #value = undefined;
-    #validator = undefined;
+  #name = "";
+  #value = undefined;
+  #validator = undefined;
 
-    get name() {
-        return this.#name;
-    }
+  get name() {
+    return this.#name;
+  }
 
-    get value() {
-        return this.#value;
-    }
+  get value() {
+    return this.#value;
+  }
 
-    get validator() {
-        return this.#validator;
-    }
+  get validator() {
+    return this.#validator;
+  }
 
-    constructor(name, value, validator) {
-        this.#name = name;
-        this.#value = value;
-        this.#validator = validator;
-    }
+  constructor(name, value, validator) {
+    this.#name = name;
+    this.#value = value;
+    this.#validator = validator;
+  }
 }
