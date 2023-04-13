f70877a08d7b37210750ae31bfcf71beddb6534e
ankoyeeri <ray.zug@gmail.com>
2023-04-13 14:01:43 +0300

    docs: leave explanation comments in home.js

--- html-css-exercise/scripts/home.js
+++ html-css-exercise/scripts/home.js
@@ -2,6 +2,7 @@ import { TableDrawer } from "./common/table.js";
 import AlertErrorHandler from "./common/error.js";
 import Movie from "./models/Movie.js";
 
+//  Array of pre-defined movies
 const movies = [
   new Movie("Batman", "Action", 4.55, 1),
   new Movie("Samurai Champloo", "Action", 4.55, 1),
@@ -9,34 +10,36 @@ const movies = [
   new Movie("Danganronpa", "Thriller", 4.55, 0),
 ];
 
+//  Array of customer rented movies
 const yourMovies = [];
 
+// Function that describes, how table rows will be drawn
 function fillRule(item, index) {
   if (!(item instanceof Movie)) throw new TypeError('"item" should be a Movie');
 
-  const row = document.createElement("tr");
+  const row = document.createElement("tr"); // Main element
   row.id = index;
 
-  const name = document.createElement("td");
+  const name = document.createElement("td"); // Name cell
   name.innerHTML = item.name;
 
-  const genre = document.createElement("td");
+  const genre = document.createElement("td"); // Genre cell
   genre.innerHTML = item.genre;
 
-  const priceFor12H = document.createElement("td");
+  const priceFor12H = document.createElement("td"); // Price for 12 Hours cell
   priceFor12H.innerHTML = item.priceFor12H;
 
-  const inStock = document.createElement("td");
+  const inStock = document.createElement("td"); //  InStock status cell
 
   if (item.inStock) {
     inStock.innerHTML =
-      '<div class="icon"><div class="icon-check"></div></div>';
+      '<div class="icon"><div class="icon-check"></div></div>'; // Positive icon display. "icon-check" CSS class should be defined
   } else {
     inStock.innerHTML =
-      '<div class="icon"><div class="icon-cross"></div></div>';
+      '<div class="icon"><div class="icon-cross"></div></div>'; // Negative icon display. "icon-cross" CSS class should be defined
   }
 
-  const button = document.createElement("td");
+  const button = document.createElement("td"); // Rent button
   button.innerHTML = `<button class="button button-default">Rent</button>`;
   button.addEventListener("click", () => {
     try {
@@ -48,11 +51,17 @@ function fillRule(item, index) {
     }
   });
 
-  row.append(name, genre, priceFor12H, inStock, button);
+  row.append(name, genre, priceFor12H, inStock, button); // Build row
 
   return row;
 }
 
+/**
+ * Rent movie assigned to a specified row
+ * @param {Movie} item Movie to rent
+ * @param {Element} row Row where the item is stored
+ * @returns Rented movie or `null`
+ */
 function rentByRow(item, row) {
   if (!(item instanceof Movie)) throw new Error('"item" should be Movie');
   if (!(row instanceof Element)) throw new Error('"row" should be Element');
