277dc7a6d398065d52274291d1a3aaf5ebd05d18
ankoyeeri <ray.zug@gmail.com>
2023-04-13 14:07:27 +0300

    style: use Prettier

--- html-css-exercise/scripts/login.js
+++ html-css-exercise/scripts/login.js
@@ -1,63 +1,67 @@
 import AlertErrorHandler from "./common/error.js";
-import {FormValidator, FormField } from "./common/form-validator.js";
+import { FormValidator, FormField } from "./common/form-validator.js";
 
 function validateRegisterForm() {
   const form = document.getElementById("register-form");
   const formData = new FormData(form);
 
-  let formValidator = new FormValidator( 
-    'Register',
-    new FormField('name', formData.get('name'), function () {
-        return this.value && this.value.length > 1;
+  let formValidator = new FormValidator(
+    "Register",
+    new FormField("name", formData.get("name"), function () {
+      return this.value && this.value.length > 1;
     }),
-    new FormField('surname', formData.get('surname'), function () {
-        return this.value.length > 1;
+    new FormField("surname", formData.get("surname"), function () {
+      return this.value.length > 1;
     }),
-    new FormField('email', formData.get('email'), function () {
-      return this.value && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g.test(this.value);
+    new FormField("email", formData.get("email"), function () {
+      return (
+        this.value &&
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g.test(this.value)
+      );
     }),
-    new FormField('emailAgain', formData.get('emailAgain'), function () {
-        return this.value && this.value === formData.get('email');
+    new FormField("emailAgain", formData.get("emailAgain"), function () {
+      return this.value && this.value === formData.get("email");
     }),
-    new FormField('password', formData.get('password'), function () {
-        return this.value && this.value.length > 7;
+    new FormField("password", formData.get("password"), function () {
+      return this.value && this.value.length > 7;
     }),
-    new FormField('passwordAgain', formData.get('passwordAgain'), function () {
-        return this.value && this.value === formData.get('password');
-    }),
+    new FormField("passwordAgain", formData.get("passwordAgain"), function () {
+      return this.value && this.value === formData.get("password");
+    })
   );
 
   try {
     formValidator.validate();
-    window.location.href = 'home.html'
+    window.location.href = "home.html";
   } catch (error) {
     AlertErrorHandler.raise(error);
   }
 }
 
 function validateLoginForm() {
-    const form = document.getElementById('sign-in-form');
-    const formData = new FormData(form);
+  const form = document.getElementById("sign-in-form");
+  const formData = new FormData(form);
 
-    let formValidator = new FormValidator(
-        'Login',
-        new FormField('email', formData.get('email'), function() {
-            return this.value && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g.test(this.value);
-        }),
-        new FormField('password', formData.get('password'), function() {
-            return this.value;
-        }),
-    );
+  let formValidator = new FormValidator(
+    "Login",
+    new FormField("email", formData.get("email"), function () {
+      return (
+        this.value &&
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g.test(this.value)
+      );
+    }),
+    new FormField("password", formData.get("password"), function () {
+      return this.value;
+    })
+  );
 
-    try {
-        formValidator.validate();
-        window.location.href = 'home.html';
-    } catch (error) {
-        AlertErrorHandler.raise(error);
-    }
+  try {
+    formValidator.validate();
+    window.location.href = "home.html";
+  } catch (error) {
+    AlertErrorHandler.raise(error);
+  }
 }
-
-
 
 window.validateLoginForm = validateLoginForm;
 window.validateRegisterForm = validateRegisterForm;
